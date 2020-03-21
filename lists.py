435045109a02a113914054e50f76c9b71725c45d
unknown <tavheeda@gmail.com>
2020-03-21 23:40:15 +0530

    Added a python file

---
+++ lists.py
@@ -0,0 +1,99 @@
+# fruits = ['apple','orange','mango','apple','grapes']
+# print(fruits)
+# print(fruits[1])
+# import math
+# for fruit in fruits:
+	# print(fruit)
+
+# numbers = [1,2,3,4,5,8,9,10]
+# print(numbers[2:5])
+# print(numbers[3:])
+# for number in numbers:
+	# print(number)
+
+# i = 0 
+# while i < len(numbers):
+# 	print(numbers[i])
+# 	i +=1 
+
+# SNAKE CASING
+# float_numbers = [12.2,34.1,22.5] 
+
+# employees = ["tanveer ahmad baba",580000.80,28]
+
+# for employee in employees:
+	# print(employee)
+
+
+# python basic data structure - sequence
+
+# subjects = ["physics","chemistry","bilogy"]
+# other_subjects = ["english","urdu"]
+# # print(subjects)
+# print(subjects[2])
+# subjects[2]="biology"
+# print(subjects)
+# subjects.append("english")
+# print(subjects)
+# subjects.insert(1,"Atlas")
+# print(subjects)
+# subjects.remove("english")
+# print(subjects)
+# print(len(subjects))
+# all_subjects = subjects + other_subjects
+# print(all_subjects)
+# repitions = subjects*3
+# print(repitions)
+
+# for i in ["physics","chemistry","bilogy"]:
+	# print(i)
+
+# INDEXING
+# print(all_subjects[3:])
+# print(all_subjects[:4])
+# print(all_subjects[2:4])
+# print(all_subjects[:-2])
+# print(all_subjects[-5:-3])
+# print(all_subjects[2:-4])
+
+# print(cmp(subjects,other_subjects))
+# print(all_subjects.count("urdu"))
+# list1 = [1,2,3,4,5]
+# list2 = ["eng","phy","chem","bio","zoo"]
+# list3 = ["tavheeda",14,90]
+# print(list1)
+# print(list2)
+# print(list3)
+# print(list1*2)
+# print(list1[:3])
+# print(list2[1:5])
+# print(list3[-3:-1])
+# list1[2]="urdu"
+# print(list1)
+# list1.append("6")
+# print(list1)
+# list1.insert(3,"his")
+# print(list1)
+# del list1[2]
+# print(list1)
+# print(list1*3)
+# print(list1)
+# 
+# list1.reverse()
+# print(list1)
+ # a=30
+ # b=3
+ # a//b=c
+ # a/b=d
+ # print(c)
+ # print(d)
+# n = 5
+# i = 0
+# while i<=n:
+# 	print(i*i)
+# 	i +=1
+
+# WRTE A PROGRAM TO CONVERT LIST INTO TUPLE
+l = [1,2,3,4]
+t = tuple(l)
+print(t)
